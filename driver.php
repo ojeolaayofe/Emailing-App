485ff2c7c28568dde9ed0e9d4c23b42880f9d49f
Ojeola Ayofe <ojeolaponle@gmail.com>
2022-02-11 16:23:32 +0100

    Updated
    The php file has been updated with a new Php pattern and a bit of JavaScript is also involved to show the registration status on the window.

--- driver.php
+++ driver.php
@@ -1,27 +1,40 @@
 <?php
 //get data from form  
-$firstname = $_POST['firstname'];
-$lastname= $_POST['lastname'];
-$address= $_POST['address'];
-$refferal= $_POST['refferal'];
-$phone= $_POST['phone'];
-$email= $_POST['email'];
-$lice= $_POST['lice'];
-$ninnumber= $_POST['ninnumber'];
-$lasdri= $_POST['lasdri'];
-$date= $_POST['date'];
-$file= $_POST['file'];
-$bname= $_POST['bname'];
-$accnumber= $_POST['accnumber'];
-$accname= $_POST['accname'];
-$to = "youremail@mail.com";
-$subject = "Mail From Driver Registration";
-$txt ="firstname = ". $firstname . "\r\n  lastname = " . $lastname . "\r\n  address = " . $address . "\r\n  refferal = " . $refferal . "\r\n  phone =  " . $phone . "\r\n  email = " . $email . "\r\n  lice = " . $lice . "\r\n  ninnumber = " . $ninnumber . "\r\n  lasdri =" . $lasdri . "\r\n  date = " . $date . "\r\n  file = ".$file ."\r\n  bname = " . $bname ."\r\n  accnumber = " . $accnumber ."\r\n  accname = " . $accname;
-$headers = "From: noreply@yoursite.com" .  "\r\n" .
-"CC: somebodyelse@example.com";
-if($email!=NULL){
-    mail($to,$subject,$txt,$headers);
+$firstname = $_REQUEST['firstname'];
+$lastname= $_REQUEST['lastname'];
+$address= $_REQUEST['address'];
+$refferal= $_REQUEST['refferal'];
+$phone= $_REQUEST['phone'];
+$email= $_REQUEST['email'];
+$lice= $_REQUEST['lice'];
+$ninnumber= $_REQUEST['ninnumber'];
+$lasdri= $_REQUEST['lasdri'];
+$date= $_REQUEST['date'];
+$file= $_REQUEST['file'];
+$bname= $_REQUEST['bname'];
+$accnumber= $_REQUEST['accnumber'];
+$accname= $_REQUEST['accname'];
+
+if(empty($firstname) || empty($lastname) || empty($address) || empty($refferal) || empty($phone) || empty($email) || empty($lice) || empty($ninnumber) || empty($lasdri) || empty($date) || empty($file) || empty($bname) || empty($accnumber) || empty($accname));
+{
+    echo "Please fill all fields";
 }
-//redirect
-header("Location:index.html");
-?>
+else{
+    mail("ojeolaponle@gmail.com","Driver Registration", $firstname , $lastname , $address , $refferal , $phone , $email , $lice , $ninnumber , $lasdri , $date , $file , $bname , $accnumber , $accname );
+    echo "<script type='text/javascript'>alert('Your Registration has been completed successfully');
+
+    window.history.log(-1);
+
+    </script>";
+}
+// $to = "youremail@mail.com";
+// $subject = "Mail From Driver Registration";
+// $txt ="firstname = ". $firstname . "\r\n  lastname = " . $lastname . "\r\n  address = " . $address . "\r\n  refferal = " . $refferal . "\r\n  phone =  " . $phone . "\r\n  email = " . $email . "\r\n  lice = " . $lice . "\r\n  ninnumber = " . $ninnumber . "\r\n  lasdri =" . $lasdri . "\r\n  date = " . $date . "\r\n  file = ".$file ."\r\n  bname = " . $bname ."\r\n  accnumber = " . $accnumber ."\r\n  accname = " . $accname;
+// $headers = "From: noreply@yoursite.com" .  "\r\n" .
+// "CC: somebodyelse@example.com";
+// if($email!=NULL){
+//     mail($to,$subject,$txt,$headers);
+// }
+// //redirect
+// header("Location:index.html");
+// ?>
